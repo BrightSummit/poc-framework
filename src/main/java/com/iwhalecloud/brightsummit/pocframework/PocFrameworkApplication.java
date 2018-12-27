f787facdc870f28ac8edc6ce6ec269ce12d03d07
yuhan <yuhan0501@sina.com>
2018-12-27 14:25:25 +0800

    项目框架首次提交

---
+++ src/main/java/com/iwhalecloud/brightsummit/pocframework/PocFrameworkApplication.java
@@ -0,0 +1,14 @@
+package com.iwhalecloud.brightsummit.pocframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocFrameworkApplication.class, args);
+	}
+
+}
+
